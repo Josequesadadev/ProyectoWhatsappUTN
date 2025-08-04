a4e6457c7ba8b7a1e3fbf7b4e5442d09750c7443
josequesada.dev <josequesadajlq@gmail.com>
2025-08-03 23:46:44 -0300

    Mejoras responsive y ajustes de CSS

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -5,18 +5,18 @@ import { contacts } from '../data';
 
 const initialMessagesByContact = {
   1: [
-    { id: 1, text: "¡Hola Lio! ¿Listo para el partido?", fromUser: true },
-    { id: 2, text: "¡Anda pa alla bobo!", fromUser: false },
+    { id: 1, text: "¡Hola Lio! ¿Listo para el partido?", fromUser: true, time: "16:00" },
+    { id: 2, text: "¡Anda pa alla bobo!", fromUser: false, time: "16:01" },
   ],
   2: [
-    { id: 1, text: "Hola Cristiano, ¿cómo va todo?", fromUser: true },
-    { id: 2, text: "¡Hola! Todo bien, entrenando a full. SIUUU", fromUser: false },
-    { id: 3, text: "SIUUUU", fromUser: true },
+    { id: 1, text: "Hola Cristiano, ¿cómo va todo?", fromUser: true, time: "15:30" },
+    { id: 2, text: "¡Hola! Todo bien, entrenando a full. SIUUU", fromUser: false, time: "15:31" },
+    { id: 3, text: "SIUUUU", fromUser: true, time: "15:32" },
   ],
   3: [
-    { id: 1, text: "Neymar, ¿vas a la fiesta?", fromUser: false },
-    { id: 2, text: "¡Ni loco me pierdo esa fiesta! Voy a decir que estoy lesionado", fromUser: true },
-    { id: 3, text: "A las 11pm, no faltes.", fromUser: false },
+    { id: 1, text: "Neymar, ¿vas a la fiesta?", fromUser: false, time: "14:10" },
+    { id: 2, text: "¡Ni loco me pierdo esa fiesta! Voy a decir que estoy lesionado", fromUser: true, time: "14:11" },
+    { id: 3, text: "A las 11pm, no faltes.", fromUser: false, time: "14:12" },
   ],
 };
 
@@ -24,7 +24,7 @@ function ChatPage() {
   const { contactId } = useParams();
   const contact = contacts.find(c => c.id === parseInt(contactId));
 
-  // Obtiene los mensajes iniciales según el contacto, o un array vacío si no hay
+
   const [messages, setMessages] = useState(
     initialMessagesByContact[contactId] || []
   );
@@ -51,15 +51,16 @@ function ChatPage() {
     <div className="chat-page-body">
       <Header contact={contact} />
       <div className="chat-messages">
-        {messages.map((message) => (
-          <div
-            key={message.id}
-            className={message.fromUser ? "message user-message" : "message"}
-          >
-            {message.text}
-          </div>
-        ))}
-      </div>
+  {messages.map((message) => (
+    <div
+      key={message.id}
+      className={message.fromUser ? "message user-message" : "message"}
+    >
+      <span>{message.text}</span>
+      <span className="message-time">{message.time}</span>
+    </div>
+  ))}
+</div>
       <form className="content-barChat" onSubmit={handleSendMessage}>
         <input
           type="text"
